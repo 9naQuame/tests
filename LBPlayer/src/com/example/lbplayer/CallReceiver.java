02c2f0fcb6c7c9122bb9bd848006670958d450fa
Nana Kwame <nanakwame123@gmail.com>
2013-07-09 15:13:16 +0000

    callReceiver test commited

---
+++ LBPlayer/src/com/example/lbplayer/CallReceiver.java
@@ -0,0 +1,33 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+
+public class CallReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        String number = "";
+        Bundle bundle = intent.getExtras();
+
+
+        if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+            // Phone is ringing
+            number = bundle.getString("incoming_number");
+
+
+        } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+            // Call received
+
+        } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+            // Call Dropped or rejected
+
+
+        }
+
+
+    }
